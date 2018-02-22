0a33c2552642d4bade8d62186d9cf1ee58d4b99c
Andrew Buckwalter <bckwalter44@gmail.com>
2018-02-22 15:45:12 -0600

    Adds comments.

--- files/test/files/Andrew/script.js
+++ files/test/files/Andrew/script.js
@@ -1,8 +1,13 @@
+//https://developers.google.com/youtube/iframe_api_reference
+
+//This code loads the IFrame Player API code asynchronously
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+//This function creates an <iframe> (and YouTube player)
+//after the API code downloads.
 
 var player;
 function onYouTubeIframeAPIReady() {
@@ -16,19 +21,17 @@ function onYouTubeIframeAPIReady() {
   		}
 	});
 }
+//The API will call this function when the video player is ready.
 
 function onPlayerReady(event) {
 	event.target.playVideo();
 }
 
-var done = false;
+//The API calls this function when the player's state changes.
 function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-  		setTimeout(stopVideo, 6000);
-  		done = true;
-	}
 }
 
+//Stops the video.
 function stopVideo() {
 	player.stopVideo();
 }
