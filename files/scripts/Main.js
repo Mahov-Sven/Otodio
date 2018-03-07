f18174fd41361e16d47e29e1c8562c575315d31b
Ethan Penn <empenn54@gmail.com>
2018-03-07 17:22:05 -0600

    Added tab tooltips

--- files/scripts/Main.js
+++ files/scripts/Main.js
@@ -2,7 +2,51 @@ $(document).ready(function () {
 
 	// ------------- INITIALIZATION STUFF -------------
 	
-	
+	// ------------- START OF JQUERY STUFF -------------
 
+	$("#TAB_VIDEO").click(() => {
+		switchTab("video");
+	});
+	
+	$("#TAB_PLAYLIST").click(() => {
+		switchTab("playlist");
+	});
+	
+	$("#TAB_SETTINGS").click(() => {
+		switchTab("settings");
+	});
+	
 	// ------------- END OF JQUERY STUFF -------------
+	
+	function switchTab(newTab){
+		$("#TAB_VIDEO").removeClass("OptionActive");
+		$("#TAB_PLAYLIST").removeClass("OptionActive");
+		$("#TAB_SETTINGS").removeClass("OptionActive");
+		
+		switch(newTab){
+		case "video":
+			switchTab_Video();
+			break;
+		case "playlist":
+			switchTab_Playlist();
+			break;
+		case "settings":
+			switchTab_Settings();
+			break;
+		default:
+			console.err(`Unknown tab: ${newTab}`);
+		}
+	}
+	
+	function switchTab_Video(){
+		$("#TAB_VIDEO").addClass("OptionActive");
+	}
+	
+	function switchTab_Playlist(){
+		$("#TAB_PLAYLIST").addClass("OptionActive");
+	}
+	
+	function switchTab_Settings(){
+		$("#TAB_SETTINGS").addClass("OptionActive");
+	}
 });
