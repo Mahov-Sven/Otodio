ab9e0e69768d4348df47e73ddd6f4837f88e9ac4
Brett Middle <brett.middle@huskers.unl.edu>
2018-03-11 13:45:40 -0500

    Implement search function

--- files/scripts/SearchBar.js
+++ files/scripts/SearchBar.js
@@ -1,11 +1,19 @@
-
-
 class SearchBar {
-    static search(type) {
-        if (arguments.length === 0 || type === 'video') {
-            // TODO YTAPI
-        } else if (type === 'playlist') {
-            // TODO go through user playlists
+    static search(query, list) {
+    	// return the list if query or list is blank
+    	if (query.length === 0 || list === []) {
+    		return list;
+    	}
+    	
+    	// begin search
+    	const result = [];
+    	query = query.toLowerCase();
+        for (const element of list) {
+        	if (element.toString().match(query).length > 0) {
+        		result.push(element);
+        	}
         }
+
+        return result;
     }
 }
